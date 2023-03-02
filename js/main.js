daa9618bbe31710e146bd88c2ce22237d7bd4797
particus17 <packDparker@gmail.com>
2023-03-02 14:09:03 -0800

    first attempt at making war work with added css styling

--- js/main.js
+++ js/main.js
@@ -28,26 +28,30 @@ function drawTwo(){
         console.log(data) //logging api data from the website.
         document.querySelector('#player').src = data.cards[0].image //taking the value of card data at index 0 (so the first card pulled goes here) 
         //and store it in the variable "#player1" from our html. Our card will now be displayed to the browser 
+
         document.querySelector('#computer').src = data.cards[1].image //taking the value of card data at index 1 (so the second card pulled goes here) 
         //and store it in the variable "#player1" from our html. Our card will now be displayed to the browser
 
         let playerVal = cardValue(data.cards[0].value) // the cardValue that is being used here comes from the conversion in the cardValue function below. This is so we can register our face cards as number values
+        
         let computerVal = cardValue(data.cards[1].value) // here we do the same thing for the second card.
+        
         const pVal = [] //this is so we can take the cards and add them to an array to be compared to cVal
+        
         const cVal = []  //this is so we can take the cards and add them to an array to be compared to pVal
         
         if(playerVal > computerVal){
             document.querySelector('h3').innerHTML = "Player Wins"     
-            playerVal = pVal.push(playerVal)
+            playerVal = pVal.push(playerVal) //I want to push the values into the pVal array so that I can compare the two arrays when determining who won war
         }
         else if(playerVal < computerVal){
              document.querySelector('h3').innerHTML = "Computer Wins"
-             computerVal = cVal.push(computerVal)
+             computerVal = cVal.push(computerVal) // Same idea here except with cVal
         }  
         else{
             document.querySelector('h3').innerHTML = "Time for WAR!"
              //write some code that tracks who wins the draw:
-            war()
+            war() // This function was created 
   
         }
     })
@@ -72,13 +76,13 @@ function drawTwo(){
     function war(pVal, cVal){
        
         if(pVal > cVal){
-            document.querySelector('h3').innerHTML = "Player Wins"
+            document.querySelector('h3').innerHTML = "Player Wins War" //if winning values in player array are greater than those in the computer array then player wins
         }
         else if(pVal < cVal){
-            document.querySelector('h3').innerHTML = "Computer Wins"
+            document.querySelector('h3').innerHTML = "Computer Wins War" //if winning values in computer array are greater than those in the computer array then computer wins
         }
         else{
-            war()
+            war() //if arrays are somehow equal then run the function again
         }
     }
 }
